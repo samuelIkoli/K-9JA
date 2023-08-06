d574870a3007d190f80e5e77f471a2d8038eea0c
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-08-06 12:26:46 +0100

    added vercel.json and urlencoded

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
